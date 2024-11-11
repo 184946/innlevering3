15c97ddb4aa8744c44b384210e89258be8ede90f
Martinh511 <moh.helland@gmail.com>
2024-11-11 17:03:18 +0100

    oppg 4

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -2,14 +2,29 @@ package no.hvl.dat100.oppgave4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-
+import java.io.IOException;
 import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave3.*;
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+		try {
+			// Oppretter en PrintWriter for å skrive til filen
+			PrintWriter writer = new PrintWriter(mappe + "/" + filnavn);
 
-		
+			// Skriver blogginnholdet til filen ved å bruke toString-metoden
+			writer.print(samling.toString());
+
+			// Lukker skriveren
+			writer.close();
+
+			// Returnerer true hvis filen ble skrevet
+			return true;
+		} catch (FileNotFoundException e) {
+			// Fanger opp eventuelle feil
+			System.out.println("Feil ved skriving til fil: " + e.getMessage());
+			return false;
+		}
 	}
 }
